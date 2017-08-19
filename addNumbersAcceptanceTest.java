5c31be976e81cd9f10ff047a0795f1ffb17d79f3
eriksown <kierson.vigilla@gmail.com>
2017-08-19 17:25:07 +0800

    Update addNumbersAcceptanceTest.java

--- addNumbersAcceptanceTest.java
+++ addNumbersAcceptanceTest.java
@@ -1,20 +1,41 @@
+package stepDefinition;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
   
 public class addNumbersAcceptanceTest {
-  public int i;
-  public int j;
+  static int i;
+  static int j; 
   
 @Test 
   public boolean isInt(String a, String b) { 
-     try {
-       i = Integer.parseInt(a);
-       j = Integer.parseInt(b);
-       Assert.assertEquals(true, isInt(i));
-       Assert.assertEquals(true, isInt(j));
-     }
-     catch(NumberFormatException er) {
-       return false;
-     }
+	
+      try {
+        setI(Integer.parseInt(a));
+        setJ(Integer.parseInt(b));
+        Assert.assertEquals(true, getI());
+        Assert.assertEquals(true, getJ());
+        
+        return true;
+      }
+      catch(NumberFormatException er) {
+        return false;
+      }
   }
+
+	public static int getI() {
+		return i;
+	}
+	
+	public void setI(int i) {
+		addNumbersAcceptanceTest.i = i;
+	}
+	
+	public static int getJ() {
+		return j;
+	}
+	
+	public void setJ(int j) {
+		addNumbersAcceptanceTest.j = j;
+	}
 }
