9989ab7da7b456840d4746e7aa525e849d9a638f
eriksown <kierson.vigilla@gmail.com>
2017-08-19 13:33:13 +0800

    Update addNumbersAcceptanceTest.java

--- addNumbersAcceptanceTest.java
+++ addNumbersAcceptanceTest.java
@@ -2,12 +2,15 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
   
 public class addNumbersAcceptanceTest {
+  public int i;
+  public int j;
+  
 @Test 
   public boolean isInt(String a, String b)
     { 
       try {
-        int i = Integer.parseInt(a);
-        int j = Integer.parseInt(b);
+        i = Integer.parseInt(a);
+        j = Integer.parseInt(b);
         Assert.assertEquals(true, isInt(i));
         Assert.assertEquals(true, isInt(j));
       }
