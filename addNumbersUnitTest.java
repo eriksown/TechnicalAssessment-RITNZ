0be0a3bdd01ec33d3e078f9337e49ca6ccbefbf3
eriksown <kierson.vigilla@gmail.com>
2017-08-19 12:12:57 +0800

    Update addNumbersUnitTest.java

--- addNumbersUnitTest.java
+++ addNumbersUnitTest.java
@@ -1,6 +1,18 @@
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 public class addTwoNumbers {
-	@Test
-	public int whenTwoNumbersAddedThenSumWillReturn () {
+@Test
+	public int whenTwoNumbersAddedThenSumWillReturn () {		
+  	@Given ('^I input two valid numbers')
+
+  	@When ('^I add the two numbers')
+
+  	@Then ('^result will be returned')
 
 	}
 
