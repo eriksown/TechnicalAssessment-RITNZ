b035b869c3faeeac6e90339911335ec9a5e6bf59
eriksown <kierson.vigilla@gmail.com>
2017-08-19 13:13:15 +0800

    Update addNumbersAcceptanceTest.java

--- addNumbersAcceptanceTest.java
+++ addNumbersAcceptanceTest.java
@@ -3,11 +3,13 @@ import org.testng.annotations.Test;
   
 public class addNumbersAcceptanceTest {
 @Test 
-  public boolean isInt(String s)
+  public boolean isInt(String a, String b)
     { 
       try {
-        int i = Integer.parseInt(s);
+        int i = Integer.parseInt(a);
+        int j = Integer.parseInt(b);
         Assert.assertEquals(true, isInt(i));
+        Assert.assertEquals(true, isInt(j));
       }
       catch(NumberFormatException er) {
         return false;
