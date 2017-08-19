ce8b513267e28618104fa73b2613cce4f163a97f
eriksown <kierson.vigilla@gmail.com>
2017-08-19 16:11:49 +0800

    Update addNumbersAcceptanceTest.java

--- addNumbersAcceptanceTest.java
+++ addNumbersAcceptanceTest.java
@@ -6,16 +6,15 @@ public class addNumbersAcceptanceTest {
   public int j;
   
 @Test 
-  public boolean isInt(String a, String b)
-    { 
-      try {
-        i = Integer.parseInt(a);
-        j = Integer.parseInt(b);
-        Assert.assertEquals(true, isInt(i));
-        Assert.assertEquals(true, isInt(j));
-      }
-      catch(NumberFormatException er) {
-        return false;
-      }
-    }
+  public boolean isInt(String a, String b) { 
+     try {
+       i = Integer.parseInt(a);
+       j = Integer.parseInt(b);
+       Assert.assertEquals(true, isInt(i));
+       Assert.assertEquals(true, isInt(j));
+     }
+     catch(NumberFormatException er) {
+       return false;
+     }
+  }
 }
