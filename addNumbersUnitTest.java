89b8a757730db44be2460bd76123d8f076366374
eriksown <kierson.vigilla@gmail.com>
2017-08-19 15:49:57 +0800

    Update addNumbersUnitTest.java

--- addNumbersUnitTest.java
+++ addNumbersUnitTest.java
@@ -2,16 +2,13 @@ import cucumber.api.java.en.When;
 import cucumber.api.java.en.Then;
 
 public class addTwoNumbers {
-	private int result;
-	
-  	@When ('^I add the two numbers$')
-		public void addInt(addNumbersAcceptanceTest.i, addNumbersAcceptanceTest.j) throws Throwable {
-			result = addNumbersAcceptanceTest.i + addNumbersAcceptanceTest.j;
-		}
-  	@Then ('^result will be returned$')
-		public int sumResult() throws Throwable {
-			return result;
-		}
+   private int result;
+   @When ('^I add the two numbers$')
+	public void addInt(addNumbersAcceptanceTest.i, addNumbersAcceptanceTest.j) throws Throwable {
+		result = addNumbersAcceptanceTest.i + addNumbersAcceptanceTest.j;
 	}
-
+   @Then ('^result will be returned$')
+	public int sumResult() throws Throwable {
+		return result;
+	}
 }
